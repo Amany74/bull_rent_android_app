3ccea07546a17564dd2cc9a46e15a8fa28accb27
Amany74 <amanybusiness74@gmail.com>
2021-12-11 10:57:17 +0200

    initial

---
+++ app/src/main/java/com/example/bull_rent_app/dashboard.java
@@ -0,0 +1,14 @@
+package com.example.bull_rent_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
